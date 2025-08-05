b6e773821d736bb884acd2b9ab7725b0abd723a3
Konor90 <konorwebdesign@gmail.com>
2025-08-05 21:01:16 +0100

    Structuration du projet

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<!-- head -->
+<?php include_once('common/head.php') ?>
+
+<body class="index-page">
+
+    <!-- header -->
+    <?php include_once('common/header.php') ?>
+
+    <main class="main">
+    
+    <section id="hero" class="hero section">
+        <h1>Bienvenue</h1>
+    </section>
+
+    </main>
+
+    <!-- footer -->
+    <?php include_once('common/footer.php') ?>
+
+    <!-- foot -->
+    <?php include_once('common/foot.php') ?>
+
+</body>
+
+</html>
